9d005d6e1174ac1e11b9d14464531c2d5ed4f7dd
oliviergagnonsimard <oliviergagnonsimard2@gmail.com>
2025-05-29 22:06:50 -0400

    créer recettes qui push vers la BD

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ helpChat = """
 
 conn = connectToDB()
 
-def SeeRecipes():
+def showClients():
     with conn.cursor() as curs:
         curs.execute(f"SELECT * FROM client")
         rows = curs.fetchall()
@@ -18,16 +18,33 @@ def SeeRecipes():
         for row in rows:
             print(row)
         print("===================================")
+
+def SeeRecipes():
+    with conn.cursor() as curs:
+        showClients()
         id = input("idClient: ")
         curs.execute(f"SELECT * FROM recette WHERE \"idClient\" = {id}")
         rows = curs.fetchall()
         curs.execute(f"SELECT name FROM client WHERE \"idClient\" = {id}")
         name = curs.fetchone()[0]
         print("===================================")
-        print(f"Voici les recettes enregistrées de {name}")
+        print(f"Voici les recettes enregistrées de {name}:")
         for row in rows:
             print(row)
 
+
+def addRecipe():
+    with conn.cursor() as curs:
+        showClients()
+        id = input("idClient: ")
+        desc = input("Description de la recette: ")
+
+        curs.execute(f"SELECT * FROM recette WHERE \"idClient\" = {id} ORDER BY \"idRecette\" DESC")
+        newRecipeId = curs.fetchone()[1]
+        newRecipeId += 1
+
+        curs.execute(f"INSERT INTO recette VALUES({id}, {newRecipeId}, \'{desc}\')")
+        confirmCommitToDB(conn)
     
 
 while True:
@@ -35,7 +52,7 @@ while True:
     ask = input("Choice: ")
 
     if ask == "1":
-        continue
+        addRecipe()
 
     if ask == "2":
         SeeRecipes()
