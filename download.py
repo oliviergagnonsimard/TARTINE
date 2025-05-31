f35c7c63978d9c2e80f36f236348dad2e1ec36b4
oliviergagnonsimard <oliviergagnonsimard2@gmail.com>
2025-05-30 22:24:59 -0400

    tt download d'un coup (HUGE!!)

--- download.py
+++ download.py
@@ -13,7 +13,7 @@ if PLATFORM == "Linux":
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-downloaded_pngs_path = DIR_PATH + SLASHS + "downloaded_pngs"
+downloaded_pngs_path = DIR_PATH + SLASHS + "circulaires"
 if not os.path.exists(downloaded_pngs_path):
     os.makedirs(downloaded_pngs_path)
 
@@ -48,7 +48,7 @@ def DownloadIMGFromWeb(url, filter, compteur, epicerie):
         downloadedFileDIR = downloaded_pngs_path + SLASHS + epicerie
         if not os.path.exists(downloadedFileDIR):
             os.makedirs(downloadedFileDIR)
-        downloadedFile = downloadedFileDIR + SLASHS + "downloaded_pngs_" + str(compteur) + ".png"
+        downloadedFile = downloadedFileDIR + SLASHS + "circulaires" + str(compteur) + ".png"
 
         with open(downloadedFile, "wb") as f:
             f.write(ans.content)
@@ -64,45 +64,57 @@ def DownloadAllIMGFromCirculaire(url, filter, epicerie):
     parsedHTML = BeautifulSoup(response.text, "html.parser")
 
     url_tags = parsedHTML.find_all("a")
-    all_urls = []
+    all_IMGurls = []
 
     for tag in url_tags:
         tag_href = tag.get("href")
         full_url = urljoin(url, tag_href)
 
-        if full_url.lower().__contains__(filter) and not full_url in all_urls:
-            all_urls.append(full_url)
+        if full_url.lower().__contains__(filter) and not full_url in all_IMGurls:
+            all_IMGurls.append(full_url)
 
     compteur = 0
-    for this_url in all_urls:
+    for this_url in all_IMGurls:
         DownloadIMGFromWeb(this_url, filter, compteur, epicerie)
         compteur +=2
-        print("======================================================================")
+        print("======================================================================") 
+
+def getAllNewUrls():
+    urlsDebase = ["https://www.circulaires.com/maxi/",
+                  "https://www.circulaires.com/supermarche-iga/",
+                  "https://www.circulaires.com/metro/",
+                  "https://www.circulaires.com/superc/",
+                  "https://www.circulaires.com/provigo/"]
+
+    all_urls = []
+    for url in urlsDebase:
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print(f"Failed to fetch the following URL: {url}")
+
+        parsedHTML = BeautifulSoup(response.text, "html.parser")
+
+        url_tags = parsedHTML.find_all("a")
+
+        for tag in url_tags:
+            tag_href = tag.get("href")
+            full_url = urljoin(url, tag_href)
+
+            if full_url.__contains__("/circulaire/") and not full_url in all_urls:
+                all_urls.append(full_url)
+        
+    return all_urls
+
+def DownloadAllCirculaires():
+    urls = getAllNewUrls()
+    for this_url in urls:
+        lastSlash = this_url.rfind("/")
+        filtre = this_url[28:lastSlash]
+        name = filtre[0:filtre.rfind("/")]
+        DownloadAllIMGFromCirculaire(url=this_url,
+                               filter=filtre,
+                               epicerie=name)
 
 
-
-#Maxi
-DownloadAllIMGFromCirculaire(url="https://www.circulaires.com/maxi/circulaire/?ref=circulaires.com&n=&p=&sname=maxi&region=&sttr=1748577600&str=4371444",
-                   filter="/maxi/circulaire/",
-                   epicerie="Maxi")
-
-#IGA              
-DownloadAllIMGFromCirculaire(url="https://www.circulaires.com/supermarche-iga/circulaire/?ref=circulaires.com&n=&p=&sname=iga&region=&sttr=1748145600&str=4370364",
-                   filter="/supermarche-iga/circulaire/",
-                   epicerie="IGA")
-
-#Metro
-DownloadAllIMGFromCirculaire(url="https://www.circulaires.com/metro/circulaire/?ref=circulaires.com&n=&p=&pr=&sname=metro&region=&sttr=1748577600&str=4371444",
-                   filter="/metro/circulaire/",
-                   epicerie="Metro")
-
-#SuperC
-DownloadAllIMGFromCirculaire(url="https://www.circulaires.com/superc/circulaire/?ref=circulaires.com&n=&p=&sname=superc&region=&sttr=1748577600&str=4371444",
-                   filter="/superc/circulaire/",
-                   epicerie="SuperC")
-
-#Walmart
-DownloadIMGFromWeb(url="https://circulaires.com/d/?sname=walmart&dpage=11&sttr=1748577600&str=4371444",
-                   filter="/walmart/",
-                   epicerie="Walmart",
-                   compteur=0)
+DownloadAllCirculaires()
