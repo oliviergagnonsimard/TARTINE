5b6ea0988fc74577dab0c140005fb207bb4c9457
oliviergagnonsimard <oliviergagnonsimard2@gmail.com>
2025-06-18 21:48:07 -0400

    ouhhh pas mal la?

--- frontend/LoginPage/app/views.py
+++ frontend/LoginPage/app/views.py
@@ -4,5 +4,7 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
