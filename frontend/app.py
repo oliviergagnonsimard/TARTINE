63f56f17296d5ddcb63e25c6431b5a3e36d2d880
oliviergagnonsimard <oliviergagnonsimard2@gmail.com>
2025-06-20 10:44:30 -0400

    dbz

--- frontend/app.py
+++ frontend/app.py
@@ -19,14 +19,6 @@ data = (
 
 name = ""
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
 @app.route('/')
 def index():
     return render_template('index.html')
