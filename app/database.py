90add6da0b86715ed75de23a9eb172a2053f928c
oliviergagnonsimard <oliviergagnonsimard2@gmail.com>
2026-05-03 16:15:07 -0400

    Fix imports

--- app/database.py
+++ app/database.py
@@ -1,10 +1,21 @@
 # Librairie pour intéragir avec notre database
 import psycopg2
+from psycopg2 import pool
 import os
 import platform
 from dotenv import load_dotenv
 
 load_dotenv()
+
+connection_pool = pool.SimpleConnectionPool(
+    1,  # min connections
+    10, # max connections
+    host=     os.environ.get("DB_HOST"),
+    port=     os.environ.get("DB_PORT"),
+    dbname=   os.environ.get("DB_NAME"),
+    user=     os.environ.get("DB_USER"),
+    password= os.environ.get("DB_PASSWORD")
+)
 
 # --- Gestion des slashs en fonction de l'OS
 PLATFORM = platform.system()
@@ -18,13 +29,10 @@ configFile = DIR_PATH + SLASHS + "dbinfo.json"
 
 # FONCTIONS ------------------------------------------------------------------------------------------
 def connectToDB():
-    return psycopg2.connect(
-        host=     os.environ.get("DB_HOST"),
-        port=     os.environ.get("DB_PORT"),
-        dbname=   os.environ.get("DB_NAME"),
-        user=     os.environ.get("DB_USER"),
-        password= os.environ.get("DB_PASSWORD")
-    )
+    return connection_pool.getconn()
+
+def releaseConn(conn):
+    connection_pool.putconn(conn)
 
 def getURI():
     user =     os.environ.get("DB_USER")
@@ -52,3 +60,4 @@ def clearDiscountDB():
         curs.execute("DELETE FROM discount")
 
     confirmCommitToDB(conn)
+    releaseConn()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from psycopg2 import pool
 from database import *
 from datetime import date, datetime, timedelta
 from r2 import imageExists
@@ -6,9 +7,8 @@ import os
 def clearConsole():
     os.system('cls')
 
-conn = connectToDB()
-
 def showClients():
+    conn = connectToDB()
     with conn.cursor() as curs:
         try:
             curs.execute(f"SELECT * FROM client")
@@ -16,27 +16,36 @@ def showClients():
         except Exception as e:
             print(f"SQL ERROR: {e}")
             conn.rollback()
+
+    releaseConn(conn)
     
     return rows
 
 def getNameFromId(idClient):
+    conn = connectToDB()
     with conn.cursor() as curs:
         try:
             curs.execute("SELECT \"firstName\", \"lastName\" FROM client WHERE \"idClient\" = %s", (idClient,))
             row = curs.fetchone()
             name = f"{row[0]} {row[1]}"
+            releaseConn(conn)
             return name
         except Exception as e:
             print(f"SQL ERROR: {e}")
             conn.rollback()
+    releaseConn(conn)
+    
 
 def getUserRecipes(idClient: int):
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("SELECT * FROM recette WHERE \"idClient\" = %s ORDER BY \"idRecette\"", (idClient,))
         rows = curs.fetchall()
+        releaseConn(conn)
         return rows
 
 def addRecipe(idClient, desc):
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("SELECT * FROM recette WHERE \"idClient\" = %s ORDER BY \"idRecette\" DESC", (idClient,))
         newRecipeId = curs.fetchone()[1]
@@ -49,8 +58,11 @@ def addRecipe(idClient, desc):
         except Exception as e:
             print(f"SQL ERROR: {e}")
             conn.rollback()
+    releaseConn(conn)
+
 
 def delRecipe(idClient, idRecette):
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("SELECT \"idRecette\" FROM recette WHERE \"idClient\" = %s AND \"idRecette\" = %s", (idClient, idRecette))
         RecipeId = curs.fetchone()[0]
@@ -62,40 +74,51 @@ def delRecipe(idClient, idRecette):
         except Exception as e:
             print(f"SQL ERROR: {e}")
             conn.rollback()
+    releaseConn(conn)
 
 def modifyRecipe(idClient, idRecette, newDesc):
+    conn = connectToDB()
     print(f"Modifying recipe {idRecette} from User {idClient}... to: {newDesc}")
     with conn.cursor() as curs:
         curs.execute("SELECT description FROM recette WHERE \"idClient\" = %s AND \"idRecette\" = %s", (idClient, idRecette))
         row = curs.fetchone()
         curs.execute("UPDATE recette SET description = %s WHERE \"idClient\" = %s AND \"idRecette\" = %s", (newDesc, idClient, idRecette))
     conn.commit()
+    releaseConn(conn)
     print(f"Recipe {idRecette} from User {idClient} has been modified")
 
 def getRecipe(idClient, idRecette):
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("SELECT * FROM recette WHERE \"idClient\" = %s AND \"idRecette\" = %s", (idClient, idRecette))
         row = curs.fetchall()
+    releaseConn(conn)
     return row
 
 def getAllEpiceries():
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("SELECT nom FROM epicerie ORDER BY \"idEpicerie\"")
         rows = curs.fetchall()
+    releaseConn(conn)
     return [row[0].lower() for row in rows]
 
 def getUserInfo(idClient):
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("SELECT *, EXTRACT(YEAR FROM AGE(\"birthDate\")) AS age FROM client WHERE \"idClient\" = %s", (idClient,))
         row = curs.fetchone()
     conn.commit()
+    releaseConn(conn)
     return row
 
 def setUserInfo(idClient, Courriel, Prénom, Nom, Birthday):
+    conn = connectToDB()
     with conn.cursor() as curs:
         curs.execute("UPDATE client SET \"email\" = %s, \"firstName\" = %s, \"lastName\" = %s, \"birthDate\" = %s WHERE \"idClient\" = %s",
                       (Courriel, Prénom, Nom, Birthday, idClient))
     conn.commit()
+    releaseConn(conn)
 
 def getFlyerWeek():
     current_date = datetime.now().weekday()
@@ -123,6 +146,7 @@ def calculate_age(born):
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))  
 
 def getLeaderboard(page=1, limit=20):
+    conn = connectToDB()
     offset = (page - 1) * limit
     with conn.cursor() as curs:
         curs.execute("""
@@ -136,4 +160,5 @@ def getLeaderboard(page=1, limit=20):
             LIMIT %s OFFSET %s
         """, (limit, offset))
         rows = curs.fetchall()
+    releaseConn(conn)
     return rows
